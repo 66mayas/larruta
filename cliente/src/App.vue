8aa09a91bccfb1dec2fdd8992bcd7c8c8bbdc529
ordoaaco <pilarico@tutanota.com>
2021-10-23 09:43:53 -0500

    logica de la funcion cargarComponente

--- cliente/src/App.vue
+++ cliente/src/App.vue
@@ -1,31 +1,58 @@
 <template>
-  <div id="appComponente">
+  <div id="appComponente" name="divPpal">
     <nav id="barraTitulo">
       <img id="imgCabecera" src="./assets/imgs/cabecera.jpg" alt="Cabecera">
       <h1 id="txtCabecera">La Ruta</h1>
     </nav>
-    <nav id="barraNavegacionPpal">
-      <button class="botonMenuPpal">Administración</button>
-      <button class="botonMenuPpal">Restaurante</button>
-      <button class="botonMenuPpal">Bar</button>
-      <button class="botonMenuPpal">Billar</button>
-      <button class="botonMenuPpal">Posada</button>
+    <nav id="barraNavegacionPpal" name="barraNavegacionPpal">
+      <button class="botonMenuPpal" name="btnMenuPpal" v-on:click="cargarComponente(0)">Administración</button>
+      <button class="botonMenuPpal" name="btnMenuPpal" v-on:click="cargarComponente(1)">Restaurante</button>
+      <button class="botonMenuPpal" name="btnMenuPpal" v-on:click="cargarComponente(2)">Bar</button>
+      <button class="botonMenuPpal" name="btnMenuPpal" v-on:click="cargarComponente(3)">Billar</button>
+      <button class="botonMenuPpal" name="btnMenuPpal" v-on:click="cargarComponente(4)">Posada</button>
     </nav>
     <div>
-      <listadoProductos/>
+      <componenteActivo/>
     </div>
   </div>
 </template>
 
 <script>
-import listadoProductos from './components/listadoProductos.vue'
-
 export default {
   name: 'AppPrincipal',
+  data(){
+    return {
+      strComponente: "./components/cpte_inicio"
+    }
+  },
   components: {
-    listadoProductos
+    componenteActivo
+  },
+  methods: {
+    cargarComponente(numComponente) {
+      switch(numComponente){
+        case 0:
+          strComponente = "./components/cpte0_administracion";
+          break;
+        case 1:
+          strComponente = "./components/cpte1_restaurante";
+          break;
+        case 2:
+          strComponente = "./components/cpte2_bar";
+          break;
+        case 3:
+          strComponente = "./components/cpte3_billar";
+          break;
+        case 4:
+          strComponente = "./components/cpte4_posada";
+          break;
+        default:
+          strComponente = "./components/cpte_inicio";
+      };
+    }
   }
 }
+  import componenteActivo from strComponente;
 </script>
 
 <style></style>
